5e9b31dca52fa200e5d14faaaa65a3d434661c48
puxf <pu.xiongfei@qq.com>
2021-10-12 15:15:10 +0800

    update config

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -15,4 +15,6 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+echo 'feeds.conf.default 增加 passwall'
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -11,4 +11,20 @@
 #
 
 # Modify default IP
+echo '修改默认IP为192.168.26.26'
 sed -i 's/192.168.1.1/192.168.26.26/g' package/base-files/files/bin/config_generate
+
+echo "修改hostname"
+sed -i "s/OpenWrt/$DEVICE_NAME/g" package/base-files/files/bin/config_generate
+
+echo "修改ssid"
+sed -i "s/OpenWrt/$DEVICE_NAME/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+echo '默认使用argon主题'
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git
+ls -la
+echo 'ls -la ../lean/luci-theme-argon'
+ls -la ../lean/luci-theme-argon
+rm -rf ../lean/luci-theme-argon
+sed -i "s/bootstrap/argon/g" feeds/luci/modules/luci-base/root/etc/config/luci
