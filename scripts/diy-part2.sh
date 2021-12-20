be7c8af5068d58c4e319ffb7cfbdfc591ddbd801
PuXiongfei <pu.xiongfei@qq.com>
2021-12-21 00:13:44 +0800

    update config

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -33,29 +33,21 @@ sed -n '/luci-theme-/p' feeds/luci/collections/luci/Makefile
 
 echo "删除 lean/luci-theme-argon"
 rm -rf package/lean/luci-theme-argon
-ls package/lean | grep *argon*
 
 echo "增加 jerrykuku/luci-theme-argon"
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-ls package | grep *argon*
 
 echo "增加 jerrykuku/luci-app-argon-config"
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
-ls package | grep *argon-config*
 
 echo "增加 xiaorouji/openwrt-passwall"
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
-ls package | grep *passwall*
 
 echo "增加 vernesong/luci-app-openclash"
 git clone --depth 1 https://github.com/vernesong/OpenClash package/OpenClash
-ls package | grep *openclash*
 
 echo "增加 Lienol/luci-app-socat"
-git clone --depth 1 https://github.com/Lienol/openwrt-package Lienol/openwrt-package
-ls -1 Lienol/openwrt-package | grep *socat*
-cp -af Lienol/openwrt-package/luci-app-socat package
-ls package | grep *socat*
+git clone --depth 1 https://github.com/Lienol/openwrt-package Lienol/openwrt-package && cp -af Lienol/openwrt-package/luci-app-socat package
 
 echo "修改DEFAULT_PACKAGES"
 sed -n '/DEFAULT_PACKAGES.router/,/^ifneq/p' include/target.mk
@@ -69,36 +61,30 @@ if [ "$CONFIG_FILE_DEVICE" = "D2" ]; then
     sed -i '/d-team_newifi-d2$/,/d-team_newifi-d2$/{s/kmod-mt7603e/kmod-mt7603/g;s/kmod-mt76x2e/kmod-mt76x2/g;s/luci-app-mtwifi//g;s/-wpad-openssl//g;s/\\/luci-app-passwall \\/g}' target/linux/ramips/image/mt7621.mk
     sed -n '/d-team_newifi-d2$/,/d-team_newifi-d2$/p' target/linux/ramips/image/mt7621.mk
     echo "修改passwall默认值"
-    sed -i '/"Include Haproxy"/,/^config/{s/arm/arm||mips||mipsel/g}' package/lean/luci-app-passwall/Makefile
-    sed -i '/"Include V2ray"/,/^config/{s/arm/arm||mips||mipsel/g}' package/lean/luci-app-passwall/Makefile
-    sed -i '/"Include V2ray-Plugin/,/^config/{s/arm/arm||mips||mipsel/g}' package/lean/luci-app-passwall/Makefile
-    sed -i '/"Include Xray"/,/^endmenu/{s/arm/arm||mips||mipsel/g}' package/lean/luci-app-passwall/Makefile
+    sed -i '/"Include Haproxy"/,/^config/{s/arm/arm||mips||mipsel/g}' package/luci-app-passwall/Makefile
+    sed -i '/"Include V2ray"/,/^config/{s/arm/arm||mips||mipsel/g}' package/luci-app-passwall/Makefile
+    sed -i '/"Include V2ray-Plugin/,/^config/{s/arm/arm||mips||mipsel/g}' package/luci-app-passwall/Makefile
+    sed -i '/"Include Xray"/,/^endmenu/{s/arm/arm||mips||mipsel/g}' package/luci-app-passwall/Makefile
     cat package/lean/luci-app-passwall/Makefile
 fi
 if [ "$CONFIG_FILE_DEVICE" = "K3" ]; then
     echo "删除 lean/k3screenctrl"
     rm -rf package/lean/k3screenctrl
-    ls package/lean | grep *k3screenctrl*
 
     echo "删除 lean/luci-app-k3screenctrl"
     rm -rf package/lean/luci-app-k3screenctrl
-    ls package/lean | grep *luci-app-k3screenctrl*
 
     echo "删除 lean/k3screenctrl_build"
     rm -rf package/lean/k3screenctrl_build
-    ls package/lean | grep *k3screenctrl_build*
 
     echo "增加 lwz322/k3screenctrl"
     git clone --depth=1 https://github.com/lwz322/k3screenctrl package/k3screenctrl
-    ls package | grep *k3screenctrl*
 
     cho "增加 lwz322/luci-app-k3screenctrl"
     git clone --depth=1 https://github.com/lwz322/luci-app-k3screenctrl package/luci-app-k3screenctrl
-    ls package | grep *luci-app-k3screenctrl*
 
     cho "增加 lwz322/k3screenctrl_build"
     git clone --depth=1 https://github.com/lwz322/k3screenctrl_build package/k3screenctrl_build
-    ls package | grep *k3screenctrl_build*
 
     echo "替换brcmfmac4366c-pcie.bin"
     md5sum $GITHUB_WORKSPACE/config/brcmfmac4366c-pcie_3.0.0.4.386.45898.bin
@@ -121,10 +107,10 @@ if [ "$CONFIG_FILE_DEVICE" = "R3G" ]; then
     sed -i '/xiaomi_mi-router-3g$/,/xiaomi_mi-router-3g$/{s/\\/luci-app-rclone luci-app-openclash luci-app-passwall luci-app-aria2 \\/g}' target/linux/ramips/image/mt7621.mk
     sed -n '/xiaomi_mi-router-3g$/,/xiaomi_mi-router-3g$/p' target/linux/ramips/image/mt7621.mk
     echo "修改passwall默认值"
-    sed -i '/"Include Haproxy"/,/^config/{s/arm/arm||mips||mipsel/g}' package/lean/luci-app-passwall/Makefile
-    sed -i '/"Include V2ray"/,/^config/{s/arm/arm||mips||mipsel/g}' package/lean/luci-app-passwall/Makefile
-    sed -i '/"Include V2ray-Plugin/,/^config/{s/arm/arm||mips||mipsel/g}' package/lean/luci-app-passwall/Makefile
-    sed -i '/"Include Xray"/,/^endmenu/{s/arm/arm||mips||mipsel/g}' package/lean/luci-app-passwall/Makefile
+    sed -i '/"Include Haproxy"/,/^config/{s/arm/arm||mips||mipsel/g}' package/luci-app-passwall/Makefile
+    sed -i '/"Include V2ray"/,/^config/{s/arm/arm||mips||mipsel/g}' package/luci-app-passwall/Makefile
+    sed -i '/"Include V2ray-Plugin/,/^config/{s/arm/arm||mips||mipsel/g}' package/luci-app-passwall/Makefile
+    sed -i '/"Include Xray"/,/^endmenu/{s/arm/arm||mips||mipsel/g}' package/luci-app-passwall/Makefile
     cat package/lean/luci-app-passwall/Makefile
     if [ -e $GITHUB_WORKSPACE/config/R3G_switch.patch ]; then
         echo "R3G_switch.patch"
