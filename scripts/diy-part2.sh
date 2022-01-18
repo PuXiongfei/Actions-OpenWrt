a4f8f4b278e85a8f9ccefd65c285f10e42c24fe9
p普雄飞 <pu.xiongfei@qq.com>
2022-01-18 19:22:15 +0800

    update config

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -64,13 +64,14 @@ git clone --depth 1 https://github.com/vernesong/OpenClash package/custom/OpenCl
 echo "修改DEFAULT_PACKAGES"
 sed -n '/DEFAULT_PACKAGES.router/,/^ifneq/p' include/target.mk
 sed -i '/DEFAULT_PACKAGES.router/,/^ifneq/{s/luci-app-autoreboot//g;s/luci-app-unblockmusic//g;s/luci-app-accesscontrol//g}' include/target.mk
-sed -i '/DEFAULT_PACKAGES.router/a\\tautomount ddns-scripts_cloudflare.com-v4 ipv6helper luci-app-argon-config luci-app-diskman luci-app-easymesh luci-app-hd-idle luci-app-samba4 luci-app-ttyd luci-app-webadmin \\' include/target.mk
+sed -i '/DEFAULT_PACKAGES.router/a\\tautomount ddns-scripts_cloudflare.com-v4 ipv6helper luci-app-argon-config luci-app-easymesh luci-app-samba4 luci-app-ttyd luci-app-webadmin \\' include/target.mk
 sed -n '/DEFAULT_PACKAGES.router/,/^ifneq/p' include/target.mk
 
 if [ "$CONFIG_FILE_DEVICE" = "D2" ]; then
     echo "修改$CONFIG_FILE_DEVICE的DEVICE_PACKAGES"
     sed -n '/d-team_newifi-d2$/,/d-team_newifi-d2$/p' target/linux/ramips/image/mt7621.mk
-    sed -i '/d-team_newifi-d2$/,/d-team_newifi-d2$/{s/kmod-mt7603e/kmod-mt7603/g;s/kmod-mt76x2e/kmod-mt76x2/g;s/luci-app-mtwifi//g;s/-wpad-openssl//g;s/\\/luci-app-mwan3helper luci-app-nfs luci-app-passwall luci-app-wireguard \\/}' target/linux/ramips/image/mt7621.mk
+    sed -i '/d-team_newifi-d2$/,/d-team_newifi-d2$/{s/kmod-mt7603e/kmod-mt7603/g;s/kmod-mt76x2e/kmod-mt76x2/g;s/luci-app-mtwifi//g;s/-wpad-openssl//g}' target/linux/ramips/image/mt7621.mk
+    sed -i '/d-team_newifi-d2$/,/d-team_newifi-d2$/{s/\\/luci-app-mwan3helper luci-app-nfs luci-app-passwall luci-app-wireguard \\/}' target/linux/ramips/image/mt7621.mk
     sed -n '/d-team_newifi-d2$/,/d-team_newifi-d2$/p' target/linux/ramips/image/mt7621.mk
     echo "修改passwall默认值"
     sed -n '/INCLUDE_Shadowsocks_Libev_Client$/,/default/p' package/custom/openwrt-passwall/luci-app-passwall/Makefile
@@ -141,7 +142,7 @@ if [ "$CONFIG_FILE_DEVICE" = "R86S" ]; then
     echo "修改target/linux/x86/Makefile"
     sed -n '/DEFAULT_PACKAGES/,/BuildTarget/p' target/linux/x86/Makefile
     sed -i '/DEFAULT_PACKAGES/,/BuildTarget/{s/autosamba//g;s/luci-app-adbyby-plus//g;s/luci-app-unblockmusic//g;s/luci-app-zerotier//g;s/luci-app-xlnetacc//g}' target/linux/x86/Makefile
-    sed -i '/DEFAULT_PACKAGES/,/BuildTarget/{s/\\/luci-app-adguardhome luci-app-aria2 luci-app-mwan3helper luci-app-docker luci-app-netdata luci-app-nfs luci-app-openclash luci-app-passwall luci-app-rclone \\/}' target/linux/x86/Makefile
+    sed -i '/DEFAULT_PACKAGES/a\luci-app-adguardhome luci-app-aria2 luci-app-mwan3helper luci-app-docker luci-app-netdata luci-app-nfs luci-app-openclash luci-app-passwall luci-app-rclone \\' target/linux/x86/Makefile
     sed -n '/DEFAULT_PACKAGES/,/BuildTarget/p' target/linux/x86/Makefile
 
     echo "修改GRUB_TITLE"
@@ -155,14 +156,14 @@ if [ "$CONFIG_FILE_DEVICE" = "R86S" ]; then
     sed -n '/QCOW2_IMAGES$/,/config/p' config/Config-images.in
 
     echo "修改TARGET_IMAGES_GZIP"
-    sed -n '/TARGET_IMAGES_GZIP$/,/default n/p' config/Config-images.in
-    sed -i '/TARGET_IMAGES_GZIP$/,/default n/{s/default n/default y}' config/Config-images.in
-    sed -n '/TARGET_IMAGES_GZIP$/,/default n/p' config/Config-images.in
+    sed -n '/TARGET_IMAGES_GZIP$/,/default/p' config/Config-images.in
+    sed -i '/TARGET_IMAGES_GZIP$/,/default/{s/default n/default y/}' config/Config-images.in
+    sed -n '/TARGET_IMAGES_GZIP$/,/default/p' config/Config-images.in
 fi
 if [ "$CONFIG_FILE_DEVICE" = "Y1" ]; then
     echo "修改$CONFIG_FILE_DEVICE的DEVICE_PACKAGES"
     sed -n '/lenovo_newifi-y1$/,/lenovo_newifi-y1$/p' target/linux/ramips/image/mt7620.mk
-    sed -i '/lenovo_newifi-y1$/,/lenovo_newifi-y1$/{/DEVICE_PACKAGES/s/$/& }' target/linux/ramips/image/mt7620.mk
+    sed -i '/lenovo_newifi-y1$/,/lenovo_newifi-y1$/{/DEVICE_PACKAGES/s/$/& luci-app-nfs/}' target/linux/ramips/image/mt7620.mk
     sed -n '/lenovo_newifi-y1$/,/lenovo_newifi-y1$/p' target/linux/ramips/image/mt7620.mk
 fi
 
