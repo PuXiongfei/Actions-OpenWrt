239ece0f46201547a34553c62c91290b32f75113
p普雄飞 <pu.xiongfei@qq.com>
2022-03-26 18:39:53 +0800

    update config

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -74,7 +74,7 @@ git clone --depth 1 https://github.com/Lienol/openwrt-package Lienol/openwrt-pac
 echo "修改DEFAULT_PACKAGES"
 sed -n '/DEFAULT_PACKAGES.router/,/^ifneq/p' include/target.mk
 sed -i '/DEFAULT_PACKAGES.router/,/^ifneq/{s/luci-app-autoreboot//g;s/luci-app-ssr-plus//g;s/luci-app-unblockmusic//g;s/luci-app-ramfree//g;s/luci-app-accesscontrol//g}' include/target.mk
-sed -i '/DEFAULT_PACKAGES.router/a\\tautomount autosamba ddns-scripts_cloudflare.com-v4 iperf3 ipv6helper luci-app-argon-config luci-app-easymesh luci-app-socat luci-app-ttyd luci-app-webadmin nano \\' include/target.mk
+sed -i '/DEFAULT_PACKAGES.router/a\\tautomount autosamba ddns-scripts_cloudflare.com-v4 iperf3 ipv6helper luci-app-argon-config luci-app-socat luci-app-ttyd luci-app-webadmin nano \\' include/target.mk
 sed -n '/DEFAULT_PACKAGES.router/,/^ifneq/p' include/target.mk
 
 if [ "$CONFIG_FILE_DEVICE" = "D2" ]; then
