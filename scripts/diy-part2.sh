3aaaefd20cae9bb7f438682542f6870c1abebcc6
p普雄飞 <pu.xiongfei@qq.com>
2022-04-23 16:29:55 +0800

    update config

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -184,9 +184,8 @@ if [ "$CONFIG_FILE_DEVICE" = "R86S" ]; then
     sed -i '/DEFAULT_PACKAGES/a\luci-app-adguardhome luci-app-aria2 luci-app-netdata luci-app-openclash luci-app-passwall luci-app-rclone tailscale \\' target/linux/x86/Makefile
     sed -n '/DEFAULT_PACKAGES/,/BuildTarget/p' target/linux/x86/Makefile
     echo "修改GRUB_TITLE"
-    sed -n '/GRUB_TITLE$/,/help$/p' config/Config-images.in
-    sed -i "/GRUB_TITLE$/,/help$/{s|\"OpenWrt\"|\"OpenWrt PuXiongfei build $(date "+%Y.%m.%d")\"|}" config/Config-images.in
-    sed -n '/GRUB_TITLE$/,/help$/p' config/Config-images.in
+    echo "CONFIG_GRUB_TITLE=\"OpenWrt PuXiongfei build $(date "+%Y.%m.%d")\"" >>.config
+    echo "CONFIG_PACKAGE_IWLWIFI_DEBUG=y" >>.config
 fi
 if [ "$CONFIG_FILE_DEVICE" = "Y1" ]; then
     echo "修改$CONFIG_FILE_DEVICE的DEVICE_PACKAGES"
